90697d313cee3b9d923bf83f37279dc7b02fede1
mmoutenot <mmoutenot@gmail.com>
2016-01-30 13:14:50 -0500

    simplify ui and make form ui

--- app/index.js
+++ app/index.js
@@ -4,95 +4,70 @@ import React, {
   Component,
   StyleSheet,
   Text,
+  TextInput,
   View
 } from 'react-native';
 import Beacons from 'react-native-ibeacon';
-import { MKTextField, MKColor } from 'react-native-material-kit';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#69D2E7',
   },
   welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    fontSize: 70,
+    textAlign: 'center'
   },
-  nameViewContainer: {
-    flexDirection: 'row',
+  formContainer: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
     alignSelf: 'stretch',
     paddingHorizontal: 40
+  },
+  label: {
+    color: 'white'
+  },
+  textInput: {
+    color: 'white',
+    height: 40,
+    fontSize: 20
+  },
+  nextText: {
+    position: 'absolute',
+    top: 30,
+    right: 10,
+    fontSize: 20,
+    color: 'white'
   }
 });
-
-const TextField = MKTextField.textfieldWithFloatingLabel()
-    .withPlaceholder('Name…')
-    .withStyle({
-      flex: 1,
-      height: 39,
-      width: 100,
-    })
-    .withTextInputStyle({
-      fontSize: 20
-    })
-    .withFloatingLabelFont({
-      fontSize: 14,
-      fontWeight: '200'
-    })
-    .build();
 
 export default function native(platform) {
 
   class Marathon extends Component {
-    constructor () {
-      super();
+    constructor (props) {
+      super(props);
       this.state = {
-        beacons: []
       };
-    }
-
-    componentWillMount () {
-      let {DeviceEventEmitter} = React;
-      let region = {
-        identifier: 'Marathon',
-        uuid: '5194a9b3-f1ef-4cd3-9aef-c2c0e227614d'
-      };
-      Beacons.requestWhenInUseAuthorization();
-      Beacons.startMonitoringForRegion(region);
-      Beacons.startRangingBeaconsInRegion(region);
-      Beacons.startUpdatingLocation();
-      DeviceEventEmitter.addListener('beaconsDidRange', this._beaconDidRange.bind(this));
-    }
-
-    _beaconDidRange (data) {
-      this.setState({ beacons: data.beacons });
     }
 
     render() {
-      if (this.state.beacons.length) {
-        const proximity = this.state.beacons[0].proximity;
-        return (
-          <View style={styles.container}>
-            <View style={styles.nameViewContainer}>
-              <TextField/>
-            </View>
-            <Text style={styles.welcome}>
-              {proximity}
-            </Text>
+      return (
+        <View style={styles.container}>
+          <Text style={styles.nextText}>Next</Text>
+          <View style={styles.formContainer}>
+            <Text style={styles.label}>Full Name</Text>
+            <TextInput
+              placeholder='John Doe'
+              placeholderTextColor={'#C8E8E3'}
+              style={styles.textInput}
+              autoFocus={true}
+            />
           </View>
-        );
-      } else {
-        return (
-          <View style={styles.container}>
-            <Text style={styles.welcome}>
-              No beacon in range!
-            </Text>
-          </View>
-        );
-      }
+          <View/>
+        </View>
+      );
     }
   }
 

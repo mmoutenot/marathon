c28b890599f984b3cdec522d37e95e0f53e4132d
mmoutenot <mmoutenot@gmail.com>
2016-01-31 18:49:15 -0500

    add fonts, and change login styles

--- app/index.js
+++ app/index.js
@@ -3,11 +3,14 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
+  StatusBarIOS,
   Text,
   TextInput,
   View
 } from 'react-native';
 import Beacons from 'react-native-ibeacon';
+
+StatusBarIOS.setHidden(true);
 
 const styles = StyleSheet.create({
   container: {
@@ -27,16 +30,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 40
   },
   label: {
+    fontFamily:'Roboto',
     color: 'white'
   },
   textInput: {
+    fontFamily:'Roboto-Light',
     color: 'white',
     height: 40,
-    fontSize: 20
+    fontSize: 28
   },
   nextText: {
+    fontFamily:'Roboto',
     position: 'absolute',
-    top: 30,
+    top: 20,
     right: 10,
     fontSize: 20,
     color: 'white'
@@ -63,6 +69,19 @@ export default function native(platform) {
               placeholderTextColor={'#C8E8E3'}
               style={styles.textInput}
               autoFocus={true}
+              autoCorrect={false}
+              returnKeyType='done'
+            />
+            <Text style={[styles.label, { marginTop: 20 }]}>Email</Text>
+            <TextInput
+              placeholder='example@email.com'
+              placeholderTextColor={'#C8E8E3'}
+              style={styles.textInput}
+              autoFocus={true}
+              autoCorrect={false}
+              keyboardType='email-address'
+              autoCapitalize='none'
+              returnKeyType='done'
             />
           </View>
           <View/>
